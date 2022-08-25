fa8679d1a3d5134d2a014e16ef941181fad4f8ad
trudyvianai <107434432+trudyvianai@users.noreply.github.com>
2022-08-25 08:14:42 -0400

    fixed drag handle positions

--- src/dBlock/DBlockNodeView.jsx
+++ src/dBlock/DBlockNodeView.jsx
@@ -2,9 +2,27 @@ import "./dBlock.css";
 import React from "react";
 import { NodeViewWrapper, NodeViewContent } from "@tiptap/react";
 export const DBlockNodeView = ({ editor }) => {
-    return (React.createElement(NodeViewWrapper, { as: "div", className: "flex gap-2 group w-full" },
-        React.createElement("section", { className: "flex mt-2 pt-[2px]", "aria-label": "left-menu", contentEditable: "false" },
-            React.createElement("div", { className: "handle-container", contentEditable: false, draggable: true, "data-drag-handle": true },
-                React.createElement("div", { className: "handle" }, "::"))),
-        React.createElement(NodeViewContent, { className: "node-view-content" })));
+  return React.createElement(
+    NodeViewWrapper,
+    { as: "div", className: "flex gap-2 items-center w-full my-4" },
+    React.createElement(
+      "section",
+      {
+        className: "flex mt-0 pt-[0px]",
+        "aria-label": "left-menu",
+        contentEditable: "false",
+      },
+      React.createElement(
+        "div",
+        {
+          className: "handle-container",
+          contentEditable: false,
+          draggable: true,
+          "data-drag-handle": true,
+        },
+        React.createElement("div", { className: "handle" }, "::")
+      )
+    ),
+    React.createElement(NodeViewContent, { className: "node-view-content" })
+  );
 };
