1dec1c269a84c81e445d4ba8046a3d0d1a993a05
Omoruyi Atekha <55259950+oeatekha@users.noreply.github.com>
2022-12-21 09:44:23 -0500

    OneWay

--- src/channel/ChannelComponent.jsx
+++ src/channel/ChannelComponent.jsx
@@ -138,14 +138,14 @@ const ChannelComponent = () => {
                 <div class="absolute inset-y-0 left-0 flex items-center pl-2  pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-4 text-gray-500 focus:outline-none dark:text-gray-400" fill="rgb(156 163 175)" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                 </div>
-                <input type="search" autocomplete="off" id="simple-search" className= "channelSearch" class="  bg-white focus:outline-0	text-gray-400 text-sm rounded-lg block w-full pl-10 p-1  dark:bg-gray-100 border-2 border-gray-50 dark:placeholder-gray-400 dark:text-gray-400 " placeholder="Search..." required/>
+                <input type="search" autocomplete="off" id="simple-search" className= "channelSearch" class="bg-white focus:outline-0	text-gray-400 text-sm rounded-lg block w-full pl-10 p-1  dark:bg-white border-2 border-gray-50 dark:placeholder-gray-400 dark:text-gray-400 " placeholder="Search..." required/>
             </div>
             
             {/*Search Input*/}
 
         </div>
         
-        <Collapsible overflowWhenOpen='visible' transitionTime='300' trigger={<hr class="py-1 mx-4 w-[654px]"></hr>}>
+        <Collapsible overflowWhenOpen='visible' transitionTime='200' trigger={<hr class="py-1 mx-4 w-[654px]"></hr>}>
           <div class="content-left container flex flex-wrap items-center justify-between w-[665px] mx-2 pb-2">
               <div className="FilterBlock">
                 {Example()}
--- src/channel/components/filterDrop.jsx
+++ src/channel/components/filterDrop.jsx
@@ -3,6 +3,8 @@ import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { useState } from 'react'
 import { filter } from '../filterUtil.js'
+import { Button } from '@chakra-ui/react'
+ 
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -13,126 +15,230 @@ function classNames(...classes) {
 // pass the function to the button
 // pass the state to the parent component
 // you have to reset the state to an empty string in the parent component once the state is passed to the parent component
+ 
+// if the filters is an array of values, then you have to pass the array to the parent component, of the filter to be added
+// f = [ [ ["t", "music"], [d], [s, 34], ] , [ ["t", "music"], [d], [s, 34], ]
+// function addFilter(f){
+
+let f = [ ["t", "Music"], ["d", "1212529"], ["author", "omo"]] //list of filters
+
+
+
+function filterValue(filter, filterList){
+    // check if filter is in list
+    for (let i = 0; i < filterList.length; i++) {
+        if (filterList[i][0] === filter[0]) {
+            return filterList[i][1]
+        }
+    }
+    return null
+}
+
+
+
+function removeFilter(curFilter, filterList){
+    // if the filter type is already in the list, we should replace it
+    // if the filter type is not in the list, we should add it
+    // imaging there is a type filter in the list we should check if the value is the same, if not add if so, replace
+    for (let i = 0; i < filterList.length; i++) {
+        if (filterList[i][0] === curFilter[0]) {
+            filterList.splice(i, 1)
+            return filterList
+        }
+    }
+    return filterList
+}
+
+function resetFilter(){
+    return []
+}
 
 
 
 
 export default function Filter() {
-    const [sortIs, setSortIs] = useState('')
-    console.log(sortIs)
 
-  return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="inline-flex w-md justify-center rounded-md border border-transparent bg-white px-2 py-2 text-sm font-medium text-gray-400 hover:bg-gray-200 focus:outline-none focus:text-gray-800 focus:bg-gray-50">
-          + Add Filter   
-        </Menu.Button>
+
+
+
+    const [filters, setFilters] = useState([])
+    console.log(setFilters)
+    let typeButton;
+    let dateButton;
+    let authorButton;
+    let supportButton;
+
+    function addFilter(curFilter){
+        // if the filter type is already in the list, we should replace it
+        // if the filter type is not in the list, we should add it
+        // imaging there is a type filter in the list we should check if the value is the same, if not add if so, replace
+        for (let i = 0; i < filters.length; i++) {
+            if (filters[i][0] === curFilter[0]) {
+                filters[i][1] = curFilter[1]
+                setFilters(filters)
+                renderFilterBar()
+                return
+            }
+        }
+
+        filters.push(curFilter)
+        filters.push(["Author", "Omo"])
+        setFilters(filters)
+        console.log("filters is ", filters)
+        renderFilterBar()
+        return
+    }
+
+    function filterinList(fil, filters){
+      for (let i = 0; i < filters.length; i++) {
+          if (filters[i][0] === fil[0]) {
+              return true
+          }
+      }
+      return false
+    }
+
+    function renderFilterBar(){
+      if(filterinList(["Type", ""], filters)) {
+        let filtVal = filterValue(["Type", ""], filters)
+        console.log("fltval is ", filtVal)
+        typeButton = <Button className='border-gray-300 p-2 font-medium rounded-lg text-sm text-gray-300'>
+         Type | {filtVal} </Button>;
+        console.log("contains type filter")
+     } else {
+        typeButton = <div></div>
+     }
+    }
+
+    renderFilterBar();
+
+
+    return (
+      // Filter List of the varuious filters, and the add filter button
+      // so the filter list will be a list of filter in the form of a list
+      // for every filter there will be a button that will remove the filter and update the filter
+
+    
+      <div className="relative pl-2 space-x-1 inline-block flex-row">
+        <div className="inline-block">
+          {typeButton}
+        </div>
+        <Menu as="div" className="relative rounded-lg inline-block text-left">
+
+        <div>
+          <Menu.Button className="inline-flex w-md justify-center rounded-md border border-transparent bg-white px-2 py-2 text-sm font-medium text-gray-400 hover:bg-gray-200 focus:outline-none focus:text-gray-400 focus:bg-gray-50">
+            + Add Filter   
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-4 py-3">
+              <p className="text-sm">Filters</p>
+            </div>
+            <div className="py-1">
+              
+              <Menu.Item onClick = {() => setFilters('type')} >
+                {({ active }) => (
+                  // when active, set the state to the prop
+                  <a
+                  
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Author
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item onClick = {() => setFilters('date')}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Date
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item onClick = {() => setFilters('date')}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Image
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item onClick = {() => setFilters('support')}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Link
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item onClick = {() => addFilter(["Type", "Text"], filters)}> 
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Text
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="py-1">
+              <form method="POST" action="#">
+              <Menu.Item onClick = {() => setFilters([])}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-red-100 text-red-900' : 'text-red-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Reset Filters
+                  </a>
+                )}
+              </Menu.Item>
+              </form>
+            </div>
+
+          </Menu.Items>
+        </Transition>
+        </Menu>
       </div>
+      
+        
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-4 py-3">
-            <p className="text-sm">Filters</p>
-          </div>
-          <div className="py-1">
-            
-            <Menu.Item onClick = {() => setSortIs('type')} >
-              {({ active }) => (
-                // when active, set the state to the prop
-                <a
-                
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Author
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item onClick = {() => setSortIs('date')}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Date
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item onClick = {() => setSortIs('date')}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Image
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item onClick = {() => setSortIs('support')}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Link
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item onClick = {() => setSortIs('author')}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Text
-                </a>
-              )}
-            </Menu.Item>
-          </div>
-          <div className="py-1">
-            <form method="POST" action="#">
-            <Menu.Item onClick = {() => setSortIs('')}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-red-100 text-red-900' : 'text-red-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Reset Filters
-                </a>
-              )}
-            </Menu.Item>
-            </form>
-          </div>
-
-        </Menu.Items>
-      </Transition>
-    </Menu>
    
   )
 }
